e7a4eec8b628ccebe92c22c34bb94298a7712eae
Kevgonz93 <161149748+Kevgonz93@users.noreply.github.com>
2024-03-19 18:28:21 +0100

    finito

--- projects/demo1/src/app/app.routes.ts
+++ projects/demo1/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main-component/main-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'home', component: MainComponent }];
--- projects/demo1/src/app/main-component/buttom/card.component.ts
+++ projects/demo1/src/app/main-component/buttom/card.component.ts
@@ -1,49 +1,20 @@
 import { Component } from '@angular/core';
-import { FormsModule, input } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'isdi-card',
+  selector: 'isdi-main',
   standalone: true,
   imports: [FormsModule],
-  template: `<li class="gentleman">
-        <div class="gentleman__avatar-container">
-          <img
-            [ngSrc]="picture"
-            alt="The Fary pointing at you"
-          />
-          <span class="gentleman__initial">F</span>
-        </div>
-        <div class="gentleman__data-container">
-          <h2> {{name}} </h2>
-          <ul class="gentleman__data-list">
-            <li class="gentleman__data">
-              <span class="gentleman__data-label">Profession:</span>
-              {{ profession }}
-            </li>
-            <li class="gentleman__data">
-              <span class="gentleman__data-label">Status:</span> {{status}}
-            </li>
-            <li class="gentleman__data">
-              <span class="gentleman__data-label">Twitter:</span> {{twitter}}
-            </li>
-          </ul>
-        </div>
-        <i class="icon gentleman__icon fas fa-check"></i>
-        <i
-          class="icon gentleman__icon gentleman__icon--delete fas fa-times"
-        ></i>
-      </li>`,
+  template: `
+  <button (click)="onClick(-1)"> - </button>
+<span>{{value}}</span>
+  <button (click)="onClick(-1)"> - </button>
+`,
   styles: ``,
 })
 
-export const gentlemans =
-  {
-    id: 1,
-    name: 'Bertin Osborne',
-    status: 'Alive',
-    profession: 'Youtuber',
-    twitter: '@osbourne',
-    picture: 'bertin.jpg',
-    alternativeText: 'Osbourne pointing at you',
-    selected: true,
-  }
+export function onClick(value:number){
+value=0
+
+  value=+1
+}
